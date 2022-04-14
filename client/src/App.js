ffaa2fde8334609e11272b9298d420e3f59d6178
voidGlitch <official.shreshthav@gmail.com>
2022-04-14 14:05:15 +0530

    seperate the components of app.js and add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,70 +1,29 @@
-import { AppBar, Container, Grid, Grow, Typography } from "@material-ui/core";
-import React, { useState, useEffect } from "react";
-import Form from "./components/form/Form";
-import Posts from "./components/posts/Posts";
-import memories from "./Images/memories.png";
-import useStyles from "./Styles";
-import { getPosts } from "./actions/posts";
-//Helps us to dispatch our action
-import { useDispatch } from "react-redux";
+import { Container } from "@material-ui/core";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 //useSelector is a function that takes the current state as an argument and returns whatever data you want from it and it allows you to store the return values inside a variable within the scope of you functional components instead of passing down as props.
 import { useSelector } from "react-redux";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
 
 const App = () => {
-  const [currentId, setCurrentId] = useState(null);
-  const classes = useStyles();
-  //Define dispatch
-  const dispatch = useDispatch();
-  //define useSelectors
+  // //define useSelectors
   const posts = useSelector((state) => state.posts);
 
   console.log(posts);
-  //Now As we want our dispact to mount manually we use useEffect
-  useEffect(() => {
-    //Dispatch Takes a Function to call
-    dispatch(getPosts());
-    //when currentId is changing after we change the post we get fresh posts
-  }, [dispatch, currentId]);
 
   return (
-    <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">
-          Memories
-        </Typography>
-        <img
-          className={classes.image}
-          src={memories}
-          alt="memories"
-          height="60"
-        />
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid
-            container
-            justifyContent="space-between"
-            alignItems="stretch"
-            spacing={3}
-            className={classes.mainContainer}
-          >
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid
-              style={{
-                maxWidth: "100%",
-              }}
-              item
-              xs={7}
-              sm={3}
-            >
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+    <BrowserRouter>
+      <Container maxWidth="lg">
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/auth" exact element={<Auth />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
 };
 
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,50 @@
+import { Container, Grid, Grow } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+//Helps us to dispatch our action
+import { useDispatch } from "react-redux";
+import Form from "../form/Form";
+import Posts from "../posts/Posts";
+import { getPosts } from "../../actions/posts";
+
+const Home = () => {
+  const [currentId, setCurrentId] = useState(null);
+
+  //Define dispatch
+  const dispatch = useDispatch();
+
+  //Now As we want our dispact to mount manually we use useEffect
+  useEffect(() => {
+    //Dispatch Takes a Function to call
+    dispatch(getPosts());
+    //when currentId is changing after we change the post we get fresh posts
+  }, [dispatch, currentId]);
+
+  return (
+    <Grow in>
+      <Container>
+        <Grid
+          container
+          justifyContent="space-between"
+          alignItems="stretch"
+          spacing={3}
+        >
+          <Grid item xs={12} sm={7}>
+            <Posts setCurrentId={setCurrentId} />
+          </Grid>
+          <Grid
+            style={{
+              maxWidth: "100%",
+            }}
+            item
+            xs={7}
+            sm={3}
+          >
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+          </Grid>
+        </Grid>
+      </Container>
+    </Grow>
+  );
+};
+
+export default Home;
