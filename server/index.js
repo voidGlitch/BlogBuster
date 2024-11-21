7904be45aa36e04ddb51f044497a8e08aed4c7b1
voidGlitch <official.shreshthav@gmail.com>
2024-11-21 15:38:23 +0530

    Made some changes in the port address

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use("/user", userRoutes);
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.CONNECTION_URL, {
+  .connect(process.env.CONN, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
